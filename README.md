ff41db171c09d1f900b83c3d1c621db99c1869cb
nogueiralhsp <nogueiralhsp@gmail.com>
2023-11-20 06:29:33 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lecture4_airline
+/**
+ * This class represents a sample code for the lecture.
+ * It demonstrates the usage of a specific feature or concept.
+ * 
+ * [Add more details about the code and its purpose here]
+ */
+public class LectureCode {
+    // Code goes here
+}
---
+++ users/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
+
+# 1234aiaiai
